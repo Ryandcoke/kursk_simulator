00144ff6335a2d2d1d56c1dd7eeed1c24ed42e8a
Evan Quan <evanlquan@gmail.com>
2018-07-14 18:33:18 -0600

    change remove

--- source/tank_components.py
+++ source/tank_components.py
@@ -7,18 +7,28 @@ class AmmoRack(object):
         self.ammo = ammo
 
     def remove(self, name):
+        """
+        name  ex:
+            "American Mark I"
+
+        Returns:
+            True if shell was removed
+        """
         # Get the number of shells of specified type
         count = self.get(name)
-        # if the rack does not contain the specified ammo type
-        # return nothing
-        if count is None:
-            return None
 
-        # If there are no more shells left, then remove shell
-        # from ammo rack
+        if count is None:
+            # if the rack does not contain the specified ammo type
+            # return nothing
+            return False
+
         new_count = count - 1
-        if new_count == 0:
-            self.ammo.remove(name)
+
+        if new_count < 0:
+            # If there are no more shells left, then remove shell
+            # from ammo rack
+            return False
         else:
             # Otherwise, we decrease the ammo count by 1
             self.ammo.put(name, new_count)
+            return True
