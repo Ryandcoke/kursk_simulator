7b1b1b69ba6468f04e253a805e7f2f8353d5899c
Evan Quan <evanlquan@gmail.com>
2018-07-15 01:12:10 -0600

    Add ShellFactory

--- source/shells.py
+++ source/shells.py
@@ -1,6 +1,6 @@
 class Shell(object):
     """
-    The shell class
+    Represents a shell to be fired
     """
     def __init__(self, name, constant, coefficient_1, coefficient_2, damage):
         self.name = name
@@ -10,8 +10,34 @@ class Shell(object):
         self.damage = damage
 
 
+class ShellFactory(object):
+    """
+    Creates shells
+
+    This is important because AmmoRack does not actually store shells but
+    the name of the shell type (String).
+    """
+    @staticmethod
+    def get_shell(name):
+        """
+        Creates a shell of a specified name
+
+        Parameters:
+            String  name:   of shell to create
+
+        Returns:
+            Shell   shell:  of specified name
+                            or None if invalid name
+        """
+        pass
+
+
 class ExplosiveShell(Shell):
     """
+
+    May or may not be needed. If the "explosive" property has the same effect
+    for all explosive shells, then we can simply add a boolean explosive
+    attribute to the Shell class.
 
     """
     def __init__(self):
